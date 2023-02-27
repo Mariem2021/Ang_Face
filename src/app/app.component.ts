2a7207ebd62feba0f93e0a51aeaf31ef3b1d80e1
Lenovo <mariemeaidara@esp.sn>
2023-02-27 18:13:32 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'MyPhoto';
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap! : FaceSnap;
+  myLastSnap! : FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'Ma meilleure amie depuis toute petite !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0
+    );
+    this.myOtherSnap = new FaceSnap (
+      'Three Rock Mountain',
+      'Un endroit magnifique',
+      'https://th.bing.com/th/id/R.d7231b7be961f8713131d8470335a8ae?rik=apiEFlPN8eebeQ&riu=http%3a%2f%2fcdn.shopify.com%2fs%2ffiles%2f1%2f0019%2f9881%2f5268%2farticles%2fNature_et_zen.com_4_1024x1024.jpg%3fv%3d1528163013&ehk=6K92%2fCb3pOYe8SECCfl5WiHvg02aagV5C9dSuQ87NHg%3d&risl=&pid=ImgRaw&r=0',
+      new Date(),
+      0
+  );
+  
+  this.myLastSnap = new FaceSnap (
+      'Three Rock Mountain',
+      'Un endroit magnifique',
+      'https://th.bing.com/th/id/R.d7231b7be961f8713131d8470335a8ae?rik=apiEFlPN8eebeQ&riu=http%3a%2f%2fcdn.shopify.com%2fs%2ffiles%2f1%2f0019%2f9881%2f5268%2farticles%2fNature_et_zen.com_4_1024x1024.jpg%3fv%3d1528163013&ehk=6K92%2fCb3pOYe8SECCfl5WiHvg02aagV5C9dSuQ87NHg%3d&risl=&pid=ImgRaw&r=0',
+      new Date(),
+      0
+  );
+  }
 }
---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {
+  @Input() faceSnap!: FaceSnap;
+  buttonText! : string;
+
+  ngOnInit() {
+    this.buttonText = 'Oh Snaps!';
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'Oh Snap!'
+    }
+  }
+
+}
